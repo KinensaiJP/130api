aef15bb5eb4558232cf27612187358b44ba2374f
kokoa0429 <kokoa@sweak.net>
2018-06-01 01:28:08 +0900

    Change something
    Change something

--- admin/changecount.php
+++ admin/changecount.php
@@ -1,21 +1,17 @@
 <?php
 require_once  __DIR__ .  '/../resource/mysql.php';
+
 try {
     $pdo = new PDO($db_host_super, $db_user, $db_password, [PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
-
 } catch(PDOException $e) {
-
   //todo
     die($e->getMessage());
-
 }
 
 if(isset($_POST['count'])) {
-
-$stmt = $pdo->prepare('UPDATE `count` SET `count` = ?');
-$stmt->bindValue(1,$_POST['count'],PDO::PARAM_INT);
-$stmt->execute();
-
+    $stmt = $pdo->prepare('UPDATE `count` SET `count` = ?');
+    $stmt->bindValue(1,$_POST['count'],PDO::PARAM_INT);
+    $stmt->execute();
 }
 
 $stmt = $pdo->query('SELECT * FROM `count`');
@@ -27,9 +23,8 @@ foreach($stmt as $row) {
 ?>
 <!DOCTYPE html>
 <html>
-<head><meta charset="UTF-8"><title>kobato prpr</title></head>
-<body>
-<style>body { overflow-x : hidden ; height:100%; width:100%; background-image:url("KBI.svg"); background-size:cover; } </style>
-<form method="post"><input id="int" name="count" type="number" value="<?= $count ?>"><input type="submit" id="btn" value="set"></form>
-</body>
+  <head><meta charset="UTF-8"><title>来場数変更</title></head>
+  <body>
+    <form method="post"><input name="count" type="number" value="<?= $count ?>"><input type="submit" value="set"></form>
+  </body>
 </html>
--- count.php
+++ count.php
@@ -3,12 +3,9 @@ require_once __DIR__ . '/resource/mysql.php';
 
 try {
     $pdo = new PDO($db_host_super, $db_user, $db_password, [PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
-
 } catch(PDOException $e) {
-    
   //todo  
     die($e->getMessage());
-
 }
 
 $count = $pdo->query('SELECT * FROM `count`');
@@ -18,9 +15,3 @@ header("Content-Type: application/json; charset=utf-8");
 foreach($count as $row) {
     echo json_encode($row);
 }
-
-
-//var_dump($count);
-
-//header("Content-Type: application/json; charset=utf-8");
-//echo "a-prpr kobatoprpr";i
